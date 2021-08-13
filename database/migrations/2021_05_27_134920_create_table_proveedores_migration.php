f7b303b23e66b91e8ccebf77201878cd3037139b
Sebastian Mon <sebamon12@gmail.com>
2021-08-13 09:32:57 -0300

    commit online

--- database/migrations/2021_05_27_134920_create_table_proveedores_migration.php
+++ database/migrations/2021_05_27_134920_create_table_proveedores_migration.php
@@ -27,6 +27,6 @@ class CreateTableProveedoresMigration extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('proveedores');
+        Schema::dropIfExists('proveedores');//prueba
     }
 }
